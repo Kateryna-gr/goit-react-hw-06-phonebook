ad065dbb27885511821b1010daa77ec99dc12a8e
Kateryna-gr <128936446+Kateryna-gr@users.noreply.github.com>
2023-11-12 18:37:46 +0200

    fixed

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -1,19 +1,31 @@
-export const Filter = ({ onFilteredName, onFilteredNumber }) => {
+import { useDispatch } from 'react-redux';
+import { changeFilterName, changeFilterNumber } from 'redux/filterSlice';
+
+export const Filter = () => {
+  const dispatch = useDispatch();
+
+  const onFilteredName = value => {
+    dispatch(changeFilterName(value));
+  };
+
+  const onFilteredNumber = value => {
+    dispatch(changeFilterNumber(value));
+  };
+
   return (
     <div>
       <p>Find contacts by name</p>
       <input
-      type="text"
-      name="filterName"
-      onChange={evt => onFilteredName(evt.target.value)}
-    />
-    <p>Find contacts by number</p>
+        type="text"
+        name="filterName"
+        onChange={evt => onFilteredName(evt.target.value)}
+      />
+      <p>Find contacts by number</p>
       <input
-      type="number"
-      
-      name="filterNumber"
-      onChange={evt => onFilteredNumber(evt.target.value)}
-    />
+        type="number"
+        name="filterNumber"
+        onChange={evt => onFilteredNumber(evt.target.value)}
+      />
     </div>
   );
 };
